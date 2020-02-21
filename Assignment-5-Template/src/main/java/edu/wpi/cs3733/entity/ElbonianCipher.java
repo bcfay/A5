465527b4e60a9399d54d07528b2ec944e79157e6
Brian Fay <bcfay@wpi.edu>
2020-02-21 17:29:10 -0500

    Finished Ciphers

--- Assignment-5-Template/src/main/java/edu/wpi/cs3733/entity/ElbonianCipher.java
+++ Assignment-5-Template/src/main/java/edu/wpi/cs3733/entity/ElbonianCipher.java
@@ -5,11 +5,38 @@ package edu.wpi.cs3733.entity;
  */
 public class ElbonianCipher implements Observer {
 
-	public void setText(String text){
+	private String text;
 
+	public void setText(String text) {
+		this.text = text;
 	}
 
+
 	public String getText(){
+
+		final int LOWERCASE_SHIFT = 96;
+		final int UPPERCASE_SHIFT = 64;
+
+
+		char[] letters = this.text.toCharArray();
+		String encodedText = "";
+
+		for (char letter : letters) {
+			if (letter > 'z' && letter < 'a') {//lowercase letters
+				letter = (char) ((int)letter - LOWERCASE_SHIFT);
+			} else if (letter > 'Z' && letter < 'A') {//uppercase letters
+				letter = (char) ((int)letter - UPPERCASE_SHIFT);
+			} else {//non-letter char
+				letter = letter;
+			}
+
+			encodedText += letter;
+		}
+
+
+
+
+
 		return null;
 	}
 
